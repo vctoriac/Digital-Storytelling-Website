e3a6bdb8d511686cf574a32211b262a6d585482f
vctoriac <victoriacheung@utexas.edu>
2024-09-17 10:39:07 -0500

    change PNG

--- sketch.js
+++ sketch.js
@@ -15,11 +15,11 @@ function setup() {
 
   // Create an upload button to add images
   let uploadButton = createFileInput(handleFile);
-  uploadButton.position(10, 10);
+  uploadButton.position(100, 60);
 
   // Add images 
-  addBackgroundImage("./images/book3.png", 0, 0);
-  addStickerImage("./images/pin.png", 100, 100);
+  addBackgroundImage("./images/book3.PNG", 30, -50);
+  addStickerImage("./images/pin.PNG", 100, 100);
   addStickerImage("./images/flower.png", 200, 200);
   addStickerImage("./images/austin.png", 300, 300);
 }
