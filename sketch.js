3657cf4e60832b5f153126b15c1790ba99bda621
vctoriac <victoriacheung@utexas.edu>
2024-09-17 05:06:26 -0500

    Simplify and comment on code

--- sketch.js
+++ sketch.js
@@ -1,38 +1,43 @@
 let drawingCanvas;
+const backgroundImages = []; 
+const userImages = [];       
+const stickerImages = [];    
 
-// Store all images with their positions and states
-const backgroundImages = [];
-const userImages = [];
-const stickerImages = [];
-
-let currentImgObj;
-let dragging = false;
-let drawing = false;
+let currentImgObj;  
+let dragging = false; 
+let drawing = false;  
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // Create the main canvas
 
-  // Create a separate drawing canvas
+  // Create a separate canvas for drawing
   drawingCanvas = createGraphics(windowWidth, windowHeight);
 
-  // Create an upload button
+  // Create an upload button to add images
   let uploadButton = createFileInput(handleFile);
   uploadButton.position(10, 10);
-  addBackgroundImage("./images/book3.PNG", 0, 0)
-  addStickerImage("./images/pin.PNG", 100, 100)
-  addStickerImage("./images/flower.png", 200, 200)
-  addStickerImage("./images/austin.png", 300, 300)
+
+  // Add images 
+  addBackgroundImage("./images/book3.png", 0, 0);
+  addStickerImage("./images/pin.png", 100, 100);
+  addStickerImage("./images/flower.png", 200, 200);
+  addStickerImage("./images/austin.png", 300, 300);
 }
 
 function draw() {
-  // Redraw background and canvases
-  background(235, 220, 210);
+  background(235, 220, 210); // Set bg color
 
-  // Draw all images
-  drawImages(backgroundImages)
-  drawImages(userImages)
+  // Draw all background images
+  drawImages(backgroundImages);
+
+  // Draw all user images
+  drawImages(userImages);
+
+  // Draw the drawing canvas on top of the main canvas
   image(drawingCanvas, 0, 0);
-  drawImages(stickerImages)
+
+  // Draw all sticker images on top
+  drawImages(stickerImages);
 }
 
 function drawImages(images) {
@@ -42,15 +47,15 @@ function drawImages(images) {
 }
 
 function mousePressed() {
-  dragging = false;
+  dragging = false; 
 
-  // Check if the mouse is over any image and enable dragging
-
-  const images = userImages.concat(stickerImages)
-  for (const imgObj of images.slice().reverse()) { // Go through the images in reverse order (top image gets priority)
-    if (mouseX > imgObj.x && mouseX < imgObj.x + imgObj.sizeX && mouseY > imgObj.y && mouseY < imgObj.y + imgObj.sizeY) {
+  // Check if the mouse is over any image
+  const images = userImages.concat(stickerImages);
+  for (const imgObj of images.slice().reverse()) {
+    if (mouseX > imgObj.x && mouseX < imgObj.x + imgObj.sizeX &&
+        mouseY > imgObj.y && mouseY < imgObj.y + imgObj.sizeY) {
       dragging = true;
-      currentImgObj = imgObj
+      currentImgObj = imgObj; 
       break;
     }
   }
@@ -62,70 +67,66 @@ function mousePressed() {
 
 function mouseDragged() {
   if (dragging) {
-    // Update position of the dragged image
+    // Move the dragged image to follow the mouse
     currentImgObj.x = mouseX - currentImgObj.sizeX / 2;
     currentImgObj.y = mouseY - currentImgObj.sizeY / 2;
   } else if (drawing) {
-    // Draw stroke on the drawing canvas
-    drawingCanvas.stroke(0);
-    drawingCanvas.strokeWeight(10);
-    drawingCanvas.line(pmouseX, pmouseY, mouseX, mouseY); // Draw a line from the previous mouse position to the current one
+    // Draw on the drawing canvas
+    drawingCanvas.stroke(0); // Set stroke color to black
+    drawingCanvas.strokeWeight(10); // Set stroke weight
+    drawingCanvas.line(pmouseX, pmouseY, mouseX, mouseY); // Draw a line from the last mouse position to the current one
   }
 }
 
 function mouseReleased() {
-  dragging = false; // Stop dragging the image
+  dragging = false; // Stop dragging when the mouse is released
   drawing = false;  // Stop drawing
 }
 
-// Function to handle uploaded files
+// Function to handle file uploads
 function handleFile(file) {
   if (file.type === 'image') {
-    let img = createImg(file.data, ''); // Create an image from the file data
-    img.hide(); // Hide the HTML element (we just need the p5 image)
-    addUserImage(file.data)
+    let img = createImg(file.data, ''); // Create an image element from the file data
+    img.hide(); // Hide the HTML element
+    addUserImage(file.data); // Add the image to user images
   } else {
     console.log('Not an image file!');
   }
 }
 
 function addBackgroundImage(filePath, x, y, sizeX, sizeY) {
-  addImage(backgroundImages, filePath, x, y, sizeX, sizeY, false)
+  addImage(backgroundImages, filePath, x, y, sizeX, sizeY, false);
 }
 
 function addUserImage(filePath, x, y, sizeX, sizeY) {
-  addImage(userImages, filePath, x, y, sizeX, sizeY, true)
+  addImage(userImages, filePath, x, y, sizeX, sizeY, true);
 }
 
 function addStickerImage(filePath, x, y, sizeX, sizeY) {
-  addImage(stickerImages, filePath, x, y, sizeX, sizeY, true)
+  addImage(stickerImages, filePath, x, y, sizeX, sizeY, true);
 }
 
 function addImage(images, filePath, x, y, sizeX, sizeY, movable = true) {
-  // Load the image and add to array of images
-  loadImage(filePath,
-    img => {
-      const imgObj = {
-        img: img,
-        movable: movable,
-        x: x ?? windowWidth / 2,
-        y: y ?? windowHeight / 2,
-        sizeX: sizeX ?? img.width,
-        sizeY: sizeY ?? img.height
-      }
-      images.push(imgObj);
-    }
-  )
+  // Load the image and add it to the array of images
+  loadImage(filePath, img => {
+    const imgObj = {
+      img: img,
+      movable: movable,
+      x: x ?? windowWidth / 2, // Default to center if not provided
+      y: y ?? windowHeight / 2,
+      sizeX: sizeX ?? img.width, // Default to image's original size if not provided
+      sizeY: sizeY ?? img.height
+    };
+    images.push(imgObj);
+  });
 }
 
 // Handle window resizing
 function windowResized() {
-  // Create a new graphics canvas to store the current drawing
+  // Create a new graphics canvas with the new window size
   let newDrawingCanvas = createGraphics(windowWidth, windowHeight);
-  // Copy the contents of the old drawing canvas onto the new one
+  // Copy the old drawing canvas content to the new one
   newDrawingCanvas.image(drawingCanvas, 0, 0);
-  // Replace the old drawing canvas with the new one
-  drawingCanvas = newDrawingCanvas;
-  // Resize the main canvas
-  resizeCanvas(windowWidth, windowHeight);
+  drawingCanvas = newDrawingCanvas; // Replace the old canvas
+  resizeCanvas(windowWidth, windowHeight); // Resize the main canvas
 }
